cf5babcea66f331b92f9ea0e23e0989165eefaff
Daniel Jenkala <djenkala@yahoo.com>
2020-02-06 20:57:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,11 @@ class Student
   def self.create_table
     sql = <<- SQL
     CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY
-      name )
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT)
+      SQL
+    end
   
   
 
