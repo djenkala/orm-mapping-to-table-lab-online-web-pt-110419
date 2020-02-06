ddfa26e34aa82731fe077b37acb7da4249ddc0c3
Daniel Jenkala <djenkala@yahoo.com>
2020-02-06 21:18:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,6 @@ class Student
     sql = <<-SQL
     DROP TABLE IF EXISTS students
     SQL
-    
     DB[:conn].execute(sql)
   end
     
@@ -33,7 +32,6 @@ class Student
     INSERT INTO students(name:, grade:)
     VALUES (?, ?)
     SQL
-    
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
