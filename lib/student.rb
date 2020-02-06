1c9dbd292f0317dceabcd3a1433ac64d57236a03
Daniel Jenkala <djenkala@yahoo.com>
2020-02-06 21:10:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,6 +37,8 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
+  def self.create
+  
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
